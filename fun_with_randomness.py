49183ed65b7ce8a31b41e3bdef7228b5800804e4
Anish Prabhu <prabhuanish@berkeley.edu>
2017-08-18 14:57:09 -0700

    Added white noise generator, included sample file in case you hit the daily quota and can't run it.

--- fun_with_randomness.py
+++ fun_with_randomness.py
@@ -3,12 +3,18 @@ random.org API (https://www.random.org/clients/http/)'''
 
 import requests
 import json
-from PIL import Image
 import time
+
+from PIL import Image
+
+import pyaudio
+import math
+import array
+import wave
 
 base_url = "https://www.random.org/"
 max_request_size = 10000
-max_failed_requests = 200
+max_failed_requests = 10
 
 def create_rgb_bitmap(width, height):
     """A function to randomly generate a rgb bitmap using PIL for easy visualization.
@@ -27,18 +33,54 @@ def create_rgb_bitmap(width, height):
     rand_vals = rand_int(num_pixels, 0, 255)
     if rand_vals == None: return None
 
+    # Create tuples of (r,g,b) values to create our image
     pixel_vals = [(rand_vals[i], rand_vals[i+1], rand_vals[i+2]) for i in range(0, num_pixels, 3)]
 
     # Create a black image with proper dimensions
     img = Image.new('RGB', (width, height))
-    pixels = img.load()
 
     # Change pixel values to our randomly generated pixel intensities
+    pixels = img.load()
     for i in range(img.size[0]):    
         for j in range(img.size[1]): 
             pixels[i,j] = pixel_vals[width*i + j]
 
     return img
+
+def create_white_noise(min_freq=400.0, max_freq=1200.0, duration=3, sampling_rate=44100):
+    """A function to generate a truly random integer.
+
+    Args:
+        min_freq (int): The minimum range of frequencies to randomly generate
+        max_freq (int): The minimum range of frequencies to randomly generate
+        duration (int): The duration in seconds of the wav file.
+        Sampling Rate (int): The sampling rate, standard is 44100
+
+    Returns:
+        None: Creates a WhiteNoise.wav file in cwd
+    
+    Source: Inspired from http://code.activestate.com/recipes/578168-sound-generator-using-wav-file/
+    """
+
+    volume = 50
+    data = array.array('h')
+    num_chan = 1
+    numSamples = sampling_rate * duration
+
+    # Limit so we don't go over our maximum allowance
+    rand_freqs = rand_int(numSamples//duration, min_freq, max_freq)
+
+    for i in range(numSamples):
+        freq = rand_freqs[i%len(rand_freqs)]
+        numSamplesPerCyc = int(sampling_rate / freq)
+        sample = 32767 * float(volume) / 100
+        sample *= math.sin(math.pi * 2 * (i % numSamplesPerCyc) / numSamplesPerCyc)
+        data.append(int(sample))
+
+    f = wave.open('WhiteNoise.wav', 'w')
+    f.setparams((num_chan, data.itemsize, sampling_rate, numSamples, "NONE", "Uncompressed"))
+    f.writeframes(data.tostring())
+    f.close()
 
 def rand_int(num_to_generate, min, max, base=10, format="plain", rnd="new"):
     """A function to generate a truly random integer.
@@ -92,13 +134,16 @@ def rand_int(num_to_generate, min, max, base=10, format="plain", rnd="new"):
     return rand_vals
 
 def main():
-    # Demo: Print out 10 with random numbers
+    # Demo: Print out 10 random numbers
     rand_numbers = rand_int(10,1,100)
     print(rand_numbers)
 
     # Demo: Create a 128x128 bitmap with randomly generated pixel values
     img = create_rgb_bitmap(128, 128)
     img.show()
+
+    # Demo: Create a .wav file with white noise
+    create_white_noise(min_freq=400, max_freq=800,duration=3)
 
 if __name__ == "__main__": main()
 
